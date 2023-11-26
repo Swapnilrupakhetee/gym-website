ef53da8617b17661f93d4a31f55ba7d235d2eb8d
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-26 20:44:27 +0545

    2nd push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Herosection from './components/Herosection.jsx/Herosection';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Herosection/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Herosection.jsx/Herosection.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import { BsSearch } from "react-icons/bs";
+import { RxHamburgerMenu } from "react-icons/rx";
+import '../../styles/herosection.css';
+
+
+function Herosection() {
+
+  
+  
+  return (
+    <div className='herosection'>
+      <div className='navlinks'>
+        <div className='links'>
+          <div className='logo'><h2>GYM FREAK</h2>
+          </div>
+          <a href='sd'className='active'>Home</a>
+          <a href='sd'className='active'>About</a>
+          <a href='sd'className='active'>Pricing</a>
+          <a href='sd'className='active'>Team</a> 
+         <BsSearch className='search' />
+         <RxHamburgerMenu className='hamburger'/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Herosection;
