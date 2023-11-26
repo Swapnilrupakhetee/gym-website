bb6654375a7ed796ca416e8049523be4ecf19669
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-26 20:50:43 +0545

    1st

--- src/components/Herosection.jsx/Herosection.jsx
+++ src/components/Herosection.jsx/Herosection.jsx
@@ -17,7 +17,7 @@ function Herosection() {
           <a href='sd'className='active'>Home</a>
           <a href='sd'className='active'>About</a>
           <a href='sd'className='active'>Pricing</a>
-          <a href='sd'className='active'>Contact</a> 
+          <a href='sd'className='active'>Contact us</a> 
          <BsSearch className='search' />
          <RxHamburgerMenu className='hamburger'/>
         </div>
