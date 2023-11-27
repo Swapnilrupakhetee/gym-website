09ac2badacd8d4aa2ec2e1d446a652046e75b7b0
Swapnil <rupakheteeswapnil@gmail.com>
2023-11-27 22:06:06 +0545

    dissappearing issue

--- src/components/Herosection.jsx/Herosection.jsx
+++ src/components/Herosection.jsx/Herosection.jsx
@@ -1,25 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import { BsSearch } from "react-icons/bs";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { ImCross } from "react-icons/im";
 import '../../styles/herosection.css';
 
 
 function Herosection() {
+  const [button, setButton] = useState(true);
+  const [ham, setHam] = useState(false); // Set initial state to false
 
-  
-  
+  const setbutton = () => {
+    setButton(!button);
+    setHam(!ham);
+  };
+
   return (
     <div className='herosection'>
       <div className='navlinks'>
         <div className='links'>
-          <div className='logo'><h2>GYM FREAK</h2>
+          <div className='logo'>
+            <h2>GYM FREAK </h2>
+            {button ? (
+              <RxHamburgerMenu className='hamburger' onClick={setbutton} />
+            ) : (
+              <ImCross className='hamburger' onClick={setbutton} />
+            )}
           </div>
-          <a href='sd'className='active'>Home</a>
-          <a href='sd'className='active'>About</a>
-          <a href='sd'className='active'>Pricing</a>
-          <a href='sd'className='active'>Contact us</a> 
-         <BsSearch className='search' />
-         <RxHamburgerMenu className='hamburger'/>
+          <>
+            {ham ? (
+              (
+                <>
+                  <a href='sd' className='active'>
+                    Home
+                  </a>
+                  <a href='sd' className='active'>
+                    About
+                  </a>
+                  <a href='sd' className='active'>
+                    Pricing
+                  </a>
+                  <a href='sd' className='active'>
+                    Contact us
+                  </a>
+                  <BsSearch className='search' />
+                </>
+              )
+            ) : <p>sadsa</p>}
+          </>
         </div>
       </div>
     </div>
@@ -27,3 +54,6 @@ function Herosection() {
 }
 
 export default Herosection;
+
+
+
