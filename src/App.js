fe8fcb8bb39b795e43fb72e6476910f844395c63
Swapnil <rupakheteeswapnil@gmail.com>
2023-12-01 21:38:35 +0545

    10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import About from './components/About';
 import Herosection from './components/Herosection.jsx/Herosection';
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Herosection/>
+      <About/>
       
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../components/About.css'
+
+function About() {
+  return (
+    <div className='about-container'>
+        <div className='about-content'>
+            <div className='items'>
+                <div className='item-title'>GET FIT WITH US</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
