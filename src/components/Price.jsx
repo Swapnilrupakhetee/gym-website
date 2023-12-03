eef50913bc3fdcbbf371bcaddfdf7f8f5dc0499e
Swapnil <rupakheteeswapnil@gmail.com>
2023-12-03 22:41:22 +0545

    pricing

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import '../components/Price.css'
+import { CgGym } from "react-icons/cg";
+import { AiFillDollarCircle } from "react-icons/ai";
 
 function Price() {
   return (
-    <div className='price-container'>Price</div>
+    <div className='price'>
+    <h1 className='pricing-title'>OUR PRICING <CgGym /></h1>
+    <div className='price-container'>
+        
+        <div className='price-options'>
+            <h2 className='price-tier'>Bronze</h2>
+            <div className='pricing-container'><AiFillDollarCircle className='dollar'/><p>30</p></div>
+
+        </div>
+        <div className='price-options'>
+            <h2 className='price-tier'>Silver</h2>
+            <div className='pricing-container'><AiFillDollarCircle className='dollar'/><p>50</p></div>
+            
+        </div>
+        <div className='price-options'>
+            <h2 className='price-tier'>Gold</h2>
+            <div className='pricing-container'><AiFillDollarCircle className='dollar'/><p>100</p></div>
+        
+        </div>
+    </div>
+    </div>
   )
 }
 
