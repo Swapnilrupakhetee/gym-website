287b20517fd936e057fcfe048eb26c9a1e3494aa
Swapnil <rupakheteeswapnil@gmail.com>
2023-12-06 23:41:50 +0545

    newsletter-finished

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -3,7 +3,16 @@ import './Newsletter.css'
 
 function Newsletter() {
   return (
-    <div className='newsletter-container'>Newsletter</div>
+    <div className='newsletter-container'>
+        <div className='newsletter-left'>
+        <p>Want product news and updates? Sign up for our newsletter</p>
+        </div>
+        <div className='newsletter-right'>
+            <input type='text' className='newsletter-container'/>
+            <div className='subscribe-btn'>Subscribe</div>
+           
+        </div>
+    </div>
   )
 }
 
